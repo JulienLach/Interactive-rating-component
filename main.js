29b83a8dab16f210a8c660de2bebf138b3e63d22
JulienLach <leadedchamps@gmail.com>
2023-11-27 14:49:29 +0100

    ok

---
+++ main.js
@@ -0,0 +1,20 @@
+// FONCTION SUBMIT QUI ENVOIE VERS THANK.HTML
+
+const btnSubmit = document.querySelector('.btn-submit');
+
+btnSubmit.addEventListener('click', function () {
+    window.location.href = 'thank.html';
+
+
+
+});
+
+
+
+// FONTION DES BOUTONS
+
+const btn1 = document.getElementById('btn-2');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btn4 = document.getElementById('btn-4');
+const btn5 = document.getElementById('btn-5');
